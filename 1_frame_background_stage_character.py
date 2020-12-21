fc4e8b8a13f1d3c91b05ccf9a536f8c5c645ef47
김현백 <>
2020-12-21 17:27:50 +0900

    weapon create

--- 1_frame_background_stage_character.py
+++ 1_frame_background_stage_character.py
@@ -35,6 +35,24 @@ character_height = character_size[1]
 character_x_pos = (screen_width / 2) - (character_width / 2)
 character_y_pos = screen_height - character_height - stage_height
 
+# 캐릭터 이동 방향
+character_to_x = 0
+
+#캐릭터 이동 속도
+character_speed = 5
+
+# 무기 만들기
+weapon = pygame.image.load(os.path.join(image_path, "weapon.png"))
+weapon_size = weapon.get_rect().size
+weapon_width = weapon_size[0]
+
+# 무기는 한 번에 여러발 발사 가능
+
+weapons = []
+
+# 무기 이동 속도
+weapon_speed = 10
+
 running = True 
 while running:
     dt = clock.tick(30) 
@@ -59,14 +77,31 @@ while running:
                 character_to_x = 0
 
     # 3. 게임 캐릭터 위치 정의
+    character_x_pos += character_to_x
+
+    if character_x_pos < 0:
+        character_x_pos = 0
+    elif character_x_pos > screen_width - character_width:
+        character_x_pos = screen_width -character_width
+
+    # 무기 위치 조정
+    weapons = [ [w[0], w[1] - weapon_speed] for w in weapons] # 무기 위치를 위로
     
+    # 천장에 닿은 무기 없애기
+    weapons = [ [w[0], w[1]] for w in weapons if w[1] > 0 ]
+
     # 4. 충돌 처리
     
-    # 5. 화면에 그리기
+    # 5. 화면에 그리기    
     screen.blit(background, (0, 0))
+
+    for weapon_x_pos, weapon_y_pos in weapons:
+        screen.blit(weapon, (weapon_x_pos, weapon_y_pos))
+
     screen.blit(stage, (0, screen_height - stage_height))
     screen.blit(character, (character_x_pos, character_y_pos))
 
     pygame.display.update() 
+
 
 pygame.quit()
